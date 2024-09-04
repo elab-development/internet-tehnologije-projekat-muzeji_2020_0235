b36e07f8ff504166999d80674daeb5a2009e76f8
ninakosutic <ninaaakosutic@gmail.com>
2024-09-04 21:26:10 +0200

    kontroler za rezervaciju

--- app/app/Http/Controllers/ReservationController.php
+++ app/app/Http/Controllers/ReservationController.php
@@ -2,83 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class ReservationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // Prikaz svih rezervacija za ulogovanog korisnika
     public function index()
     {
-        //
+        $reservations = Reservation::where('user_id', Auth::id())->get();
+        return response()->json($reservations, 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    // Prikaz jedne rezervacije
     public function show($id)
     {
-        //
+        $reservation = Reservation::findOrFail($id);
+        return response()->json($reservation, 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    // Kreiranje nove rezervacije
+    public function store(Request $request)
     {
-        //
+        // Validacija polja
+        $validator = Validator::make($request->all(), [
+            'museum_id' => 'required|exists:museums,id',
+            'reservation_date' => 'required|date',
+            'num_tickets' => 'required|integer|min:1',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        // Kreiranje nove rezervacije
+        $reservation = Reservation::create([
+            'user_id' => Auth::id(),  // Koristi ID ulogovanog korisnika
+            'museum_id' => $request->input('museum_id'),
+            'reservation_date' => $request->input('reservation_date'),
+            'num_tickets' => $request->input('num_tickets'),
+        ]);
+
+        return response()->json($reservation, 201);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    // Ažuriranje postojeće rezervacije
     public function update(Request $request, $id)
     {
-        //
+        // Pronalaženje rezervacije
+        $reservation = Reservation::findOrFail($id);
+
+        // Validacija polja
+        $validator = Validator::make($request->all(), [
+            'museum_id' => 'required|exists:museums,id',
+            'reservation_date' => 'required|date',
+            'num_tickets' => 'required|integer|min:1',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        // Ažuriranje rezervacije
+        $reservation->update($request->all());
+
+        return response()->json($reservation, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    // Brisanje rezervacije
     public function destroy($id)
     {
-        //
+        // Pronalaženje rezervacije
+        $reservation = Reservation::findOrFail($id);
+
+        // Brisanje rezervacije
+        $reservation->delete();
+
+        return response()->json(['message' => 'Reservation deleted successfully'], 200);
     }
 }
