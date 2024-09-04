3c513220b262aba0c88a6091d9521a3e7d173535
ninakosutic <ninaaakosutic@gmail.com>
2024-09-04 21:25:04 +0200

    api rute za muzeje

--- app/routes/api.php
+++ app/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MuseumController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,15 @@ use App\Http\Controllers\AuthController;
 |
 */
 
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/museums', [MuseumController::class, 'index']);       // Prikaz svih muzeja
+    Route::get('/museums/{id}', [MuseumController::class, 'show']);   // Prikaz jednog muzeja
+    Route::post('/museums', [MuseumController::class, 'store']);      // Kreiranje novog muzeja
+    Route::put('/museums/{id}', [MuseumController::class, 'update']); // Ažuriranje muzeja
+    Route::delete('/museums/{id}', [MuseumController::class, 'destroy']); // Brisanje muzeja
+});
 
 
 Route::post('/register', [AuthController::class, 'register']);
