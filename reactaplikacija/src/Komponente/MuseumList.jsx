4c170b7d48e6ac66bf99f6006b8dbd3f7a538d65
ninakosutic <ninaaakosutic@gmail.com>
2024-09-08 23:57:12 +0200

    sortiranje i pretraga muzeja

--- reactaplikacija/src/Komponente/MuseumList.jsx
+++ reactaplikacija/src/Komponente/MuseumList.jsx
@@ -7,6 +7,8 @@ const MuseumList = () => {
   const [museums, setMuseums] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState(''); // Pretraga
+  const [sortOption, setSortOption] = useState(''); // Sortiranje
 
   useEffect(() => {
     const fetchMuseums = async () => {
@@ -26,17 +28,60 @@ const MuseumList = () => {
     fetchMuseums();
   }, []);
 
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSort = (e) => {
+    setSortOption(e.target.value);
+  };
+
+  // Filtriranje muzeja na osnovu pojma za pretragu
+  const filteredMuseums = museums.filter((museum) =>
+    museum.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  // Sortiranje muzeja
+  const sortedMuseums = filteredMuseums.sort((a, b) => {
+    if (sortOption === 'name') {
+      return a.name.localeCompare(b.name);
+    } else if (sortOption === 'price') {
+      return a.ticket_price - b.ticket_price;
+    }
+    return 0;
+  });
+
   if (loading) return <p>Učitavanje muzeja...</p>;
   if (error) return <p>{error}</p>;
 
   return (
+    <>
+          {/* Pretraga */}
+          <div className="search-sort-container">
+          <input
+            type="text"
+            placeholder="Pretraži muzeje..."
+            value={searchTerm}
+            onChange={handleSearch}
+            className="search-input"
+          />
+  
+          {/* Sortiranje */}
+          <select value={sortOption} onChange={handleSort} className="sort-select">
+            <option value="">Sortiraj po</option>
+            <option value="name">Nazivu</option>
+            <option value="price">Ceni ulaznice</option>
+          </select>
+        </div>
     <div className="museum-list-container">
-      {museums.length > 0 ? (
-        museums.map((museum) => <MuseumCard key={museum.id} museum={museum} />)
+
+
+      {sortedMuseums.length > 0 ? (
+        sortedMuseums.map((museum) => <MuseumCard key={museum.id} museum={museum} />)
       ) : (
         <p>Nema dostupnih muzeja.</p>
       )}
-    </div>
+    </div></>
   );
 };
 
